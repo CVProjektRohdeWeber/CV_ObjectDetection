54457a3a415e11ce8793ddc6eac28c660cfbbd5f
rohdem <marius.rohde@gmx.de>
2014-11-12 09:18:00 +0100

    init

---
+++ HumanDetection/HumanDetection/Features.py
@@ -0,0 +1,17 @@
+import sys
+import time
+import cv2
+import Image
+import numpy as np
+import cv2.cv as cv
+
+
+def getPositiveFeatures(path):
+
+    print "test"
+
+
+def getRandomNegativeFeatures(path):
+
+    print "test"
+  
---
+++ HumanDetection/HumanDetection/HumanDetection.py
@@ -0,0 +1,10 @@
+import sys
+import Features
+
+
+if __name__ == '__main__':
+
+  if len(sys.argv) > 1:
+    path = sys.argv[1]
+
+  Features.getPositiveFeatures("")
