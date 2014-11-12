af6d8d4f79efadc238d9db3fb8887288b3861334
rohdem <marius.rohde@gmx.de>
2014-11-12 11:58:02 +0100

    calc hogfeatures

--- HumanDetection/HumanDetection/Features.py
+++ HumanDetection/HumanDetection/Features.py
@@ -1,17 +1,67 @@
 import sys
+import os
 import time
 import cv2
-import Image
+import random
 import numpy as np
 import cv2.cv as cv
 
 
 def getPositiveFeatures(path):
 
-    print "test"
+    imageFiles = os.listdir(path)
+    features = []
+
+    for imgName in imageFiles:    
+            
+        fullImgName = path+'\\'+imgName
+        image = openPositiveImage(fullImgName)
+        features.append(calcHog(image))
+
+    return features
 
 
-def getRandomNegativeFeatures(path):
+     
+def getRandomNegativeFeatures(path, amount, size):
 
-    print "test"
-  
+    imageFiles = os.listdir(path)
+    features = []
+    images = []
+
+    for imgName in imageFiles:        
+        fullImgName = path+'\\'+imgName
+        images.append(openNegativeImage(fullImgName,size))
+        images.append(openNegativeImage(fullImgName,size))
+        for image in images:
+            features.append(calcHog(image)) 
+
+    return features
+
+
+
+def openPositiveImage(fullImgName):
+
+    npImage = cv2.imread(fullImgName,0)
+
+    return npImage
+
+
+
+def openNegativeImage(fullImgName, size):
+    npImage = cv2.imread(fullImgName,cv2.CV_LOAD_IMAGE_COLOR)
+    imgX = npImage.shape[0]
+    imgY = npImage.shape[1]  
+    x = random.randint(0, imgX - size[0])
+    y = random.randint(0, imgY - size[1])
+    crop_img = npImage[x:y, size[0]:size[1]]
+    return crop_img
+
+
+
+def calcHog(image):
+
+    hog = cv2.HOGDescriptor()
+    h = hog.compute(image)
+
+    return h
+
--- HumanDetection/HumanDetection/HumanDetection.py
+++ HumanDetection/HumanDetection/HumanDetection.py
@@ -1,10 +1,19 @@
 import sys
+import os
 import Features
-
+import numpy as np
+import cv2
 
 if __name__ == '__main__':
 
   if len(sys.argv) > 1:
     path = sys.argv[1]
 
-  Features.getPositiveFeatures("")
+  currentFolder = os.getcwd() 
+  print "Start reading positive images"
+  #Features.getPositiveFeatures(currentFolder+'\\PositiveImages')
+  print "Start reading negative images"
+  size = [96,160]
+  #Features.getRandomNegativeFeatures(currentFolder+'\\NegativeImages',2,size)
+  npImage = cv2.imread(currentFolder+'\\NegativeImages\\00000002a.png')
+  cv2.imshow( "Display window", npImage )
