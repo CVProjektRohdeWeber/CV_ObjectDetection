05036c0180a787f295250af6f7d7e8425aca98a3
rohdem <marius.rohde@gmx.de>
2014-11-19 16:48:11 +0100

    init branch

--- HumanDetection/HumanDetection/DetectSVM.py
+++ HumanDetection/HumanDetection/DetectSVM.py
@@ -23,6 +23,8 @@ def detectHumans(img,svm,slidingWindowSize):
     img  = cv2.resize(img,(0,0),fx = 0.5, fy=0.5)
     cv2.imshow("kleiner",img)
     cv2.waitKey(0)
+
+
     xMax = img.shape[0] - slidingWindowSize[0]
     yMax = img.shape[1] - slidingWindowSize[1]
 
