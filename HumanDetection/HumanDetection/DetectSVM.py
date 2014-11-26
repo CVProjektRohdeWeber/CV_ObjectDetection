16508313ef4ee61215f5576c70af3cad9171b51c
w3bba <alexwebba@gmx.de>
2014-11-26 19:33:56 +0100

    Multithreading steht

--- HumanDetection/HumanDetection/DetectSVM.py
+++ HumanDetection/HumanDetection/DetectSVM.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+import threading
+import Queue
 
 
 def loadSVM(path,name):
@@ -17,40 +19,71 @@ def detectHumans(img,svm,slidingWindowSize):
     print 'start detect humans'
 
     detections = []
+
     factor = 1.0
     imgResized  = img
+    threads = []
+    returnqueue = Queue.Queue()
 
     while imgResized.shape[0] >= slidingWindowSize[0] and imgResized.shape[1] >= slidingWindowSize[1]:
 
         print imgResized.shape[0]
         print imgResized.shape[1]
-
-        xMax = imgResized.shape[0] - slidingWindowSize[0]
-        yMax = imgResized.shape[1] - slidingWindowSize[1]
-    
-        for i in range(0,xMax,10):
-            for j in range(0,yMax,10):
-                slide = imgResized[i:i+slidingWindowSize[0],j:j+slidingWindowSize[1]]
-
-                hog = cv2.HOGDescriptor((slidingWindowSize[1],slidingWindowSize[0]), (16,16), (8,8), (8,8), 9)
-                h = hog.compute(slide)
-
-                detected = svm.predict(h,True)
-                detected = detected / factor
-                if detected < - 0.7187:
-                    rect = []
-                    rect[:] = j / factor , i / factor , (j+slidingWindowSize[1]) / factor, (i+slidingWindowSize[0]) / factor, factor, detected
-                    detections.append(rect)
-                    print detected
-                    cv2.imshow("detect",slide)
-                    cv2.waitKey(1)
-
-        factor = factor-0.2
+        t = threading.Thread(None,detectThread,None,(imgResized,svm,slidingWindowSize,factor,returnqueue))
+        threads.append(t)
+        t.start()
+        factor = factor-0.1
         imgResized  = cv2.resize(img,(0,0),fx = factor, fy=factor)
+    for t in threads:
+        t.join()
+
+    print returnqueue.unfinished_tasks
+    while returnqueue.unfinished_tasks>0:
+        item = returnqueue.get()
+        print item
+        detections.append(item)
+        returnqueue.task_done()
+        print returnqueue.unfinished_tasks
 
     detections.sort(key=lambda x: x[5])
-    print "Median="+str(detections[len(detections)//2][5])
+    sum = 0.0
+    for item in detections:
+        sum += item[5]
+    avg = sum/len(detections)
+    print str(avg)
+    for item in detections:
+        if (avg>item[5]):
+            detections.remove(item)
+    print str(len(detections))
     return detections
+
+
+    
+
+def detectThread(imgResized,svm,slidingWindowSize,factor,returnqueue):
+    detections = []
+    print "Thread started!"
+    xMax = imgResized.shape[0] - slidingWindowSize[0]
+    yMax = imgResized.shape[1] - slidingWindowSize[1]
+    
+    for i in range(0,xMax,10):
+        for j in range(0,yMax,10):
+            slide = imgResized[i:i+slidingWindowSize[0],j:j+slidingWindowSize[1]]
+            
+            hog = cv2.HOGDescriptor((slidingWindowSize[1],slidingWindowSize[0]), (16,16), (8,8), (8,8), 9)
+            h = hog.compute(slide)
+
+            detected = svm.predict(h,True)
+            detected = detected / factor
+            if detected < - 1.0:
+                rect = []
+                rect[:] = j / factor , i / factor , (j+slidingWindowSize[1]) / factor, (i+slidingWindowSize[0]) / factor, factor, detected
+                returnqueue.put(rect)
+                print detected
+                cv2.imshow("detect",slide)
+                cv2.waitKey(1)
+    print "Thread done!"
+    
 
 def suppress(detections):
     new_detections = []
@@ -65,6 +98,7 @@ def suppress(detections):
 
 #  Felzenszwalb et al.
 def non_max_suppression_slow(detections, overlapThresh):
+    print "start NMS"
     detections = np.array(detections)
 
     if detections.shape[0] == 0:
@@ -125,6 +159,7 @@ def non_max_suppression_slow(detections, overlapThresh):
         idxs = np.delete(idxs, suppress)
 
     # return only the bounding boxes that were picked
+    print "NMS done!"
     return detections[pick]
 
 def detectHumansTest(img):
